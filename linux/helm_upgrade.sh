654cf99f6035fa9b3137ff94666eb96e3fbd2871
jaska <pusanteemu@gmail.com>
2018-03-25 18:26:51 +0300

    Ympäristömuuttujien asetus ja vaihdettu tiedostonimi

--- linux/helm_upgrade.sh
+++ linux/helm_upgrade.sh
@@ -5,4 +5,8 @@ RELEASE_NAME=$(echo "$CI_COMMIT_REF_NAME" | sed 's/\./-/g')
 helm upgrade \
 --wait \
 --set ristoImage=jaska/risto:$IMAGE_TAG \
+--set mysqlDatabase=$MYSQL_DATABASE \
+--set mysqlPassword=$MYSQL_PASSWORD \
+--set mysqlHost=$MYSQL_HOST \
+--set mysqlPort=$MYSQL_PORT \
 --install risto-$RELEASE_NAME ./deployment
