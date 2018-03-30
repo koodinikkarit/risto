addd1283b741da9ae0ec9e07f034944028a8ab36
jaska <pusanteemu@gmail.com>
2018-03-30 12:57:25 +0300

    Muokattu deployment asetuksia.

--- linux/helm_upgrade.sh
+++ linux/helm_upgrade.sh
@@ -5,8 +5,8 @@ RELEASE_NAME=$(echo "$TRAVIS_BRANCH" | sed 's/\./-/g')
 helm upgrade \
 --wait \
 --set ristoImage=jaska/risto:$IMAGE_TAG \
+--set mysqlUsername=$MYSQL_USERNAME \
 --set mysqlDatabase=$MYSQL_DATABASE \
 --set mysqlPassword=$MYSQL_PASSWORD \
 --set mysqlHost=$MYSQL_HOST \
---set mysqlPort=$MYSQL_PORT \
 --install risto-$RELEASE_NAME ./deployment
